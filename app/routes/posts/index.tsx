8d16c7489b65f80afab13c13a73beebfdd0b5b65
mrsekut <k.cloudspider@gmail.com>
2023-03-02 16:58:59 +0900

    initial commit

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,43 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import { useLoaderData } from "react-router";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+export const loader: LoaderFunction = () => {
+  return json({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+  console.log(posts);
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
