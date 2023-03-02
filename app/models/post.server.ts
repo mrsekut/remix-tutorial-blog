be17ecd52df8fd3199c192fc6b1ebebf3c5087b4
mrsekut <k.cloudspider@gmail.com>
2023-03-02 17:57:22 +0900

    add action

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,9 +1,5 @@
 import { prisma } from "~/db.server";
-
-export type Post = {
-  slug: string;
-  title: string;
-};
+import type { Post } from "@prisma/client";
 
 export async function getPosts() {
   return prisma.post.findMany();
@@ -11,4 +7,10 @@ export async function getPosts() {
 
 export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
 }
