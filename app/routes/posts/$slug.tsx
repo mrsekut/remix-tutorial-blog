c10fff23e76365045eccfd745c00d985eef441e9
mrsekut <k.cloudspider@gmail.com>
2023-03-02 17:44:58 +0900

    nesting routes

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { marked } from "marked";
 import invariant from "tiny-invariant";
 
 import { getPost } from "~/models/post.server";
@@ -11,14 +12,16 @@ export const loader = async ({ params }: LoaderArgs) => {
   const post = await getPost(params.slug);
   invariant(post, `Post not found: ${params.slug}`);
 
-  return json({ post });
+  const html = marked(post.markdown);
+  return json({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData<typeof loader>();
+  const { post, html } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
